9dea03eee60c9187950fd35e47a7a06df947f30f
ShibinaNoushad <shibinanoushad2224@gmail.com>
2023-01-12 21:50:57 +0530

    Movie posing and deleting added

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Movie.module.css';
+import classes from "./Movie.module.css";
 
 const Movie = (props) => {
+  const deleteMovies = async () => {
+    await fetch(
+      `https://react-moviesapp-default-rtdb.asia-southeast1.firebasedatabase.app/movies/${props.id}.json`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    props.recall();
+  };
+
   return (
     <li className={classes.movie}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <button
+        style={{ backgroundColor: "white", color: "blue" }}
+        onClick={deleteMovies}
+      >
+        Delete Movie
+      </button>
     </li>
   );
 };
